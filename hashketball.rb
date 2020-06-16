b38ef0fbaa2ca207a6e29e6f5398dd64d5cc9aae
Emily Braman <embraman10@gmail.com>
2020-06-16 16:42:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,21 @@
 # Write your code here!
+def game_hash 
+  {
+    "Brooklyn Nets" => {
+      :status => "home",
+      :team_colors => ["black", "white"]
+      :players => [
+        {:name => "Alan Anderson", 
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 1, 
+          :slam_dunks => 1
+        }
+          
+      ]
+    }
+  }
